83c7987a5b4c43a15f556859cd012749b2dfca9a
abdullah dağlı <abdullah.dagli@outlook.com>
2022-06-12 18:04:55 +0300

    Add project files.

---
+++ DataAccess/Abstract/IEntityRepository.cs
@@ -0,0 +1,26 @@
+﻿using Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+	//generic constraint - cenerik kısıt demek
+	// class : Referans tip
+	// IEntity : IEntity olabilir veya IEntiy implemente eden bir nesne olabilir.
+	//new() : new'lenebilir olmalı,
+		public interface IEntityRepository<T> where T : class , IEntity, new()
+	{
+
+		List<T> GetAll(Expression<Func<T, bool>> filter =null); // bir listelemenin hepsini getirebilir. tümünü getir
+		T Get(Expression<Func<T, bool>> filter); // burada filter yapmak zorunda
+		void Add(T entity);
+		void Update(T entity);
+		void Delete(T entity);
+
+		// List<T> GetAllByCategory(int categoryId); 
+		// buradaki koda asla ihtiyacımız olmayacak Expression methodunun filter ı ekledikten sonra
+
+	}
+}
---
+++ DataAccess/Abstract/IProductDal.cs
@@ -0,0 +1,16 @@
+﻿using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.Abstract
+{
+	public interface IProductDal:IEntityRepository<Product>
+	{
+		
+
+
+		//Interface'lerin Default methodları Public'tir.
+		
+	}
+}
